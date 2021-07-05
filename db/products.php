bd82cf4ef1788bd18b35f59b05563f97abfafbe8
peppe-p <ombrachiara96@gmail.com>
2021-07-05 16:57:27 +0200

    add html with stamp of products

--- db/products.php
+++ db/products.php
@@ -1,6 +1,9 @@
 <?php
 
 /* Classe prodotto generale */
+
+use Product as GlobalProduct;
+
 class Product
 {
     protected $name;
@@ -16,4 +19,29 @@ class Product
         $this->price = $price;
         $this->qnt = $qnt;
     }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function getQnt()
+    {
+        return $this->qnt;
+    }
 }
+
+$products = [
+    new Product('Pc Lenovo', 'Lorem ipsum dolor sit amet.', 299.89, 139),
+    new Product('Scrivania', 'Lorem ipsum dolor sit amet.', 580.99, 154),
+    new Product('Pendrive Sony', 'Lorem ipsum dolor sit amet.', 19.00, 54),
+    new Product('iPhone Apple', 'Lorem ipsum dolor sit amet.', 1299.99, 23),
+    new Product('Tastiera Gaming', 'Lorem ipsum dolor sit amet.', 199.99, 3544)
+];
--- index.php
+++ index.php
@@ -3,3 +3,51 @@
 require './db/cards.php';
 require './db/products.php';
 require './db/users.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    section {
+        width: 100%;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        padding: 100px 0;
+    }
+
+    .card {
+        padding: 20px;
+        background-color: red;
+        color: white;
+        margin: 20px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+    </style>
+    <title>OOP 2</title>
+</head>
+
+<body>
+    <h1>I nostri prodotti</h1>
+    <section>
+        <?php foreach ($products as $product) : ?>
+        <div class="card">
+            <h2><?= $product->getName() ?></h2>
+            <p><?= $product->getDesc() ?></p>
+            <h3><?= $product->getPrice() ?></h3>
+            <h3><?= $product->getQnt() ?></h3>
+        </div>
+        <?php endforeach ?>
+    </section>
+</body>
+
+</html>
